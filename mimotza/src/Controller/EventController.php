1ec96968ae8d1f76f5b65260814325bf048c902f
François-Nicolas Gitzhofer <fngitzhofersds@gmail.com>
2022-04-21 09:14:56 -0400

    Ajout et optimisation du système d'envoi d'un événement

--- mimotza/src/Controller/EventController.php
+++ mimotza/src/Controller/EventController.php
@@ -4,6 +4,13 @@ namespace App\Controller;
 
 use Doctrine\Persistence\ManagerRegistry;
 
+use App\Entity\Utilisateur;
+use App\Entity\Evenement;
+use App\Entity\Historique;
+
+use App\Repository\UtilisateurRepository;
+use App\Repository\EvenementRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,19 +21,54 @@ class EventController extends AbstractController
     #[Route('/event', name: 'app_event')]
     public function event(Request $request, ManagerRegistry $doctrine): Response
     {
-        $entityManager = $doctrine->getManager();
-
-        $eventRepos = $entityManager->getRepository();
-
-        $event;      // $event sera une entité
+        $response = "";
 
         if ($request->isMethod('post')) {
 
-            // Ajouter les méthodes pour ajouter un événement
+            // Récupère le post
+            $post = $request->request->all();
 
-            $eventRepos->add($event);
+            // Instancie l'entity manager
+            $entityManager = $doctrine->getManager();
 
-            return new RedirectResponse('/index');
+            // Instancie les repos
+            $userRepos = $entityManager->getRepository(Utilisateur::class);
+            $eventTypeRepos = $entityManager->getRepository(Evenement::class);
+
+            // Instancie l'utilisateur et le type d'événement à partir des id
+            $user = $userRepos->findOneBy(['id' => $post['userId']]);
+            $eventType = $eventTypeRepos->findOneBy(['id' => $post['eventType']]);
+
+            // Si l'utilisateur est inexistant
+            if (!$user) {
+                http_response_code(400);
+                $response = '{"erreur": "Aucun utilisateur a l\'identifiant ' . $post['userId'] . '"}';
+            }
+            // Sinon si l'événement n'existe pas
+            elseif (!$eventType) {
+                http_response_code(400);
+                $response = '{"erreur": "Aucun type d\'événement a l\'identifiant ' . $post['eventType'] . '"}';
+            }
+            else {
+                // Créé l'événement à partir du post reçu
+                $event = new Historique;
+
+                // Place les infos dans l'événement
+                $event->setIdUser($user)
+                    ->setIdEvent($eventType)
+                    ->setDetail($post['mess'])
+                    ->setDateEmission($post['date']);
+
+                // Sauvegarde l'événement
+                $entityManager->persist($event);
+                $entityManager->flush();
+
+                $response = $post;
+            }
+            
+            // Affiche le message d'erreur
+            echo $response;
+            exit;
         }
 
         return $this->render('event/event.html.twig');
