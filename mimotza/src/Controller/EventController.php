36c75ce82f3ef0113644f1ceaa0b28f9700828ec
François-Nicolas Gitzhofer <fngitzhofersds@gmail.com>
2022-04-20 21:57:52 -0400

    Ajout du squelette de la fonction event (en attente d'un merge avant de continuer)

--- mimotza/src/Controller/EventController.php
+++ mimotza/src/Controller/EventController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,9 +12,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class EventController extends AbstractController
 {
     #[Route('/event', name: 'app_event')]
-    public function event(Request $request): Response
+    public function event(Request $request, ManagerRegistry $doctrine): Response
     {
-        
+        $entityManager = $doctrine->getManager();
+
+        $eventRepos = $entityManager->getRepository();
+
+        $event;      // $event sera une entité
+
+        if ($request->isMethod('post')) {
+
+            // Ajouter les méthodes pour ajouter un événement
+
+            $eventRepos->add($event);
+
+            return new RedirectResponse('/index');
+        }
 
         return $this->render('event/event.html.twig');
     }
