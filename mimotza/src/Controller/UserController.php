a5f4152145f64b56011d90a5c349c77bab25f721
vortydev <etienne.dmenard@gmail.com>
2022-04-21 10:59:38 -0400

    formulaire basique d'inscription + temp data view

--- mimotza/src/Controller/UserController.php
+++ mimotza/src/Controller/UserController.php
@@ -3,16 +3,59 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/user', name: 'user')]
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/inscription', name: 'inscription')]
+    public function inscription(): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('prenom', TextType::class, ['label' => 'Prénom'])
+            ->add('nom', TextType::class, ['label' => 'Nom'])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ])
+            ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
+            ->add('mdp', PasswordType::class, ['label' => 'Mot de passe'])
+            ->add('submit', SubmitType::class, ['label' => 'S\'inscrire!'])
+            ->setAction($this->generateUrl('confirmation'))
+            ->getForm();
+
+        return $this->renderForm('user/inscription.html.twig', [
+            'controller_name' => 'UserController',
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/confirmation', name: 'confirmation')]
+    public function confirmation() : Response {
+        $request = Request::createFromGlobals();
+        $form = $request->get('form');
+
+        return $this->render('user/confirmation.html.twig', [
+            'form' => $form,
         ]);
     }
 }
