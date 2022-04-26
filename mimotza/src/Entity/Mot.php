eaf7759e530fac7463181751769bd1c7bf22ecca
IsabelleRioux <samoyede2003@gmail.com>
2022-04-26 13:03:44 -0400

    Ajout d'un champ mot pour les parties sur la base de données

--- mimotza/src/Entity/Mot.php
+++ mimotza/src/Entity/Mot.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MotRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MotRepository::class)]
@@ -22,6 +24,14 @@ class Mot
 
     #[ORM\Column(type: 'datetime')]
     private $dateAjout;
+
+    #[ORM\OneToMany(mappedBy: 'mot', targetEntity: Partie::class)]
+    private $parties;
+
+    public function __construct()
+    {
+        $this->parties = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -60,6 +70,36 @@ class Mot
     public function setDateAjout(\DateTimeInterface $dateAjout): self
     {
         $this->dateAjout = $dateAjout;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Partie>
+     */
+    public function getParties(): Collection
+    {
+        return $this->parties;
+    }
+
+    public function addParty(Partie $party): self
+    {
+        if (!$this->parties->contains($party)) {
+            $this->parties[] = $party;
+            $party->setMot($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParty(Partie $party): self
+    {
+        if ($this->parties->removeElement($party)) {
+            // set the owning side to null (unless already changed)
+            if ($party->getMot() === $this) {
+                $party->setMot(null);
+            }
+        }
 
         return $this;
     }
