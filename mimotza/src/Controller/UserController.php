5c575946b155ad1a808e5d61906ece22635c3473
vortydev <etienne.dmenard@gmail.com>
2022-04-26 10:33:49 -0400

    user can be inserted from json array

--- mimotza/src/Controller/UserController.php
+++ mimotza/src/Controller/UserController.php
@@ -23,6 +23,7 @@ use App\Entity\Statut;
 use App\Repository\StatutRepository;
 use App\Entity\Utilisateur;
 use App\Repository\UtilisateurRepository;
+use Exception;
 
 class UserController extends AbstractController
 {
@@ -55,6 +56,7 @@ class UserController extends AbstractController
         ]);
     }
 
+    // DEPRECATED
     #[Route('/inscription', name: 'inscription')]
     public function inscription(ManagerRegistry $doctrine): Response
     {
@@ -81,37 +83,94 @@ class UserController extends AbstractController
 
     #[Route('/adduser', name: 'adduser')]
     public function addUser(Request $request, ManagerRegistry $doctrine): Response {
-        // get post
-        $post = $request->request->all();
+        // get post TEMP
+        // $post = $request->request->all();
+        // $encode = json_encode(array($post['form'], $post['form']));
 
         // init managers
         $entityManager = $doctrine->getManager();
         $roleManager = $entityManager->getRepository(Role::class);
         $statutManager = $entityManager->getRepository(Statut::class);
+        $userManager = $entityManager->getRepository(Utilisateur::class);
 
         // generate objects
         $roleUsager = $roleManager->findOneBy(['id' => 1]);
         $statutInactif = $statutManager->findOneBy(['id' => 1]);
-        $user = new Utilisateur;
 
-        // load user data
-        $user->setPrenom($post['form']['prenom'])
-            ->setNom($post['form']['nom'])
-            ->setEmail($post['form']['email'])
-            ->setUsername($post['form']['username'])
-            ->setMdp(password_hash($post['form']['mdp'], PASSWORD_DEFAULT))
-            ->setAvatar(null)
-            ->setIdRole($roleUsager)
-            ->setIdStatut($statutInactif)
-            ->setDateCreation(date_create_from_format('Y-m-d H:i:s', date('Y-m-d H:i:s')));
+        // TEMP
+        $liste = array(
+            array(
+                'prenom' => 'Étienne',
+                'nom' => 'Ménard',
+                'email' => 'etienne.dmenard@gmail.com',
+                'username' => 'vorty',
+                'mdp' => 'abc123',
+                'role' => 2,
+                'statut' => 2,
+            ),
+            array(
+                'prenom' => 'Isabelle',
+                'nom' => 'Rioux',
+                'email' => 'isabelle.rioux@gmail.com',
+                'username' => 'isa',
+                'mdp' => 'abc123'
+            ),
+        );
 
-        // save user
-        $entityManager->persist($user);
-        $entityManager->flush();
+        $json = json_encode($liste);
+
+        // TODO get json array
+        // $data = json_decode($request->getContent(), true);
+        $data = json_decode($json, true);
+
+        // loop through array and load create each user
+        foreach ($data as $u) {
+            $emailCheck = $userManager->findOneBy(['email' => $u['email']]);
+            $usernameCheck = $userManager->findOneBy(['username' => $u['username']]);
+
+            if ($emailCheck == null && $usernameCheck == null) {
+                $user = new Utilisateur();
+
+                // load user data
+                $user->setPrenom($u['prenom'])
+                ->setNom($u['nom'])
+                ->setEmail($u['email'])
+                ->setUsername($u['username'])
+                ->setMdp(password_hash($u['mdp'], PASSWORD_DEFAULT))
+                ->setAvatar(null)
+                ->setDateCreation(date_create_from_format('Y-m-d H:i:s', date('Y-m-d H:i:s')));
+
+                // set role
+                if (!empty($u['role']) && $roleManager->findOneBy(['id' => $u['role']]) != null) {
+                    $user->setIdRole($roleManager->findOneBy(['id' => $u['role']]));
+                }
+                else {
+                    $user->setIdRole($roleUsager);
+                }
+                
+                // set statut
+                if (!empty($u['statut']) && $statutManager->findOneBy(['id' => $u['statut']]) != null) {
+                    $user->setIdStatut($statutManager->findOneBy(['id' => $u['statut']]));
+                }
+                else {
+                    $user->setIdStatut($statutInactif);
+                }
+
+                if (!empty($u['avatar'])) {
+                    $user->setAvatar($u['avatar']);
+                }
+
+                // save user
+                $entityManager->persist($user);
+            }
+        }
+
+        // push to bd
+        $entityManager->flush();        
 
         return $this->render('user/confirmation.html.twig', [
-            'controller_name' => $user->getUsername(),
-            'form' => $post['form'],
+            'controller_name' => 'poggers',
+            // 'form' => $post['form'],
         ]);
     }
 }
